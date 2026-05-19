b37d7ec3303ba28a4e6f92cf04b4ac680a5f7cdb
austinpower1258 <austinwang115@gmail.com>
2026-05-19 05:35:54 -0700

    fix: reapply ghostty runtime scheme on theme reload

--- Sources/GhosttyTerminalView.swift
+++ Sources/GhosttyTerminalView.swift
@@ -3020,20 +3020,16 @@ class GhosttyApp {
 
     enum RuntimeColorSchemeSynchronizationDecision: Equatable {
         case apply
-        case skipAlreadyApplied
         case skipReentrant
     }
 
     static func runtimeColorSchemeSynchronizationDecision(
-        applied: ghostty_color_scheme_e?,
-        requested: ghostty_color_scheme_e,
+        applied _: ghostty_color_scheme_e?,
+        requested _: ghostty_color_scheme_e,
         isSynchronizing: Bool
     ) -> RuntimeColorSchemeSynchronizationDecision {
         if isSynchronizing {
             return .skipReentrant
-        }
-        if applied == requested {
-            return .skipAlreadyApplied
         }
         return .apply
     }
@@ -3347,8 +3343,6 @@ class GhosttyApp {
                 switch decision {
                 case .apply:
                     reason = "apply"
-                case .skipAlreadyApplied:
-                    reason = "already_applied"
                 case .skipReentrant:
                     reason = "reentrant"
                 }
